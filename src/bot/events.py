54ef7b806df628e49cf0dd1516d70e61f93d4505
Neelotpal Dutta <143270070+21f3002397@users.noreply.github.com>
2025-09-09 09:58:16 +0530

    Added TODO comments in /src/bot/events.py

--- src/bot/events.py
+++ src/bot/events.py
@@ -12,7 +12,25 @@ class BotEvents(commands.Cog):
 
   @commands.Cog.listener()
   async def on_message(self, message: discord.Message) -> None:
+    #Log the message, print also works but we have logger module
     print(message)
+
+    # Check if the author of the message is a bot, if yes return, unless you want boo to interact with booette
+    # Then make the check so that, it doesn't respond to itself, but yeah can lead to a feedback loop between boo and booette.
+
+    # Prep the contents of the message to be prompt compliant for the llm.
+
+    # Based on server load the system prompt, this corresponds server_lore, or maybe for now hardcode if you plan to only have it in VVIP server.
+
+    # OPTIONAL: reset chat check, could be a slash command too.
+
+    # OPTIONAL: Image processing
+
+    # Set up the actual query to send to the llm api, and then call the llm api.
+
+    # OPTIONAL: Process the llm api output
+
+    # Pass the message back to the chat, preferrably as a reply.
 
 async def setup(bot: commands.Bot) -> None:
   await bot.add_cog(BotEvents(bot))
